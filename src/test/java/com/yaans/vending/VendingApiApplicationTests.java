092df8cbfaa1ed1a3d0a9b3da572caab03985e18
KOO-YS <kbeeysk@naver.com>
2021-09-07 18:00:52 +0900

    build test issue close

--- src/test/java/com/yaans/vending/VendingApiApplicationTests.java
+++ src/test/java/com/yaans/vending/VendingApiApplicationTests.java
@@ -1,9 +1,12 @@
 package com.yaans.vending;
 
+import com.yaans.vending.config.JasyptConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = JasyptConfig.class)
 class VendingApiApplicationTests {
 
 
